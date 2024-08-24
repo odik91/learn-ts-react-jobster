0a43a81547a543e9c31b7aa3c6f9cfa7b074b721
odik91 <ali.shoddiqien@gmail.com>
2024-08-25 00:40:42 +0700

    adding big navbar with menus funtionality

--- src/components/BigSidebar.tsx
+++ src/components/BigSidebar.tsx
@@ -1,8 +1,26 @@
-import Wrapper from "../assets/wrappers/BigSidebar"
+import Wrapper from "../assets/wrappers/BigSidebar";
+import { useAppSelector } from "../hooks/userCustomHook";
+import Logo from "./Logo";
+import NavLinks from "./NavLinks";
 
 const BigSidebar = () => {
+  const { isSidebarOpen } = useAppSelector((store) => store.user);
+
   return (
-    <Wrapper>BigSidebar</Wrapper>
-  )
-}
-export default BigSidebar
+    <Wrapper>
+      <div
+        className={
+          isSidebarOpen ? "sidebar-container" : "sidebar-container show-sidebar"
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+export default BigSidebar;
